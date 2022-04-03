56c46532b7a7a20e3f1e870fa7a964e4426a2aa9
marmooo <marmooo@users.noreply.github.com>
2022-04-03 13:27:13 +0900

    change dependencies

--- src/ttf2svg.js
+++ src/ttf2svg.js
@@ -1,30 +1,119 @@
-const fontCarrier = require("font-carrier");
+const opentype = require("opentype.js");
+const svgpath = require("svgpath");
+
+function svgHeader(font, glyph) {
+  const height = font.ascender - font.descender;
+  const copyright = fontToCopyright(font);
+  return `<svg xmlns="http://www.w3.org/2000/svg"
+  width="100" height="100"
+  viewBox="0 0 ${glyph.advanceWidth} ${height}">
+  <!--
+${copyright}
+  -->
+`;
+}
+
+function toSVG(font, glyph) {
+  const d = svgpath(glyph.path.toPathData())
+    .scale(1, -1)
+    .translate(0, font.ascender)
+    .toString();
+  const path = `<path d="${d}"/>`;
+  return svgHeader(font, glyph) + path + "\n</svg>";
+}
+
+function getInfo(hash) {
+  const arr = Object.values(hash);
+  if (arr.length > 0) {
+    return arr[0];
+  } else {
+    return "";
+  }
+}
+
+function fontToCopyright(font) {
+  const copyrights = Object.values(font.names.copyright).join("\n");
+  const trademarks = Object.values(font.names.trademark).join("\n");
+  const licenses = Object.values(font.names.license).join("\n");
+  const licenseURLs = Object.values(font.names.licenseURL).join("\n");
+  return [copyrights, trademarks, licenses, licenseURLs].join("\n");
+}
+
+function glyphHeader(font) {
+  const copyright = fontToCopyright(font);
+  const header = `<svg xmlns="http://www.w3.org/2000/svg"
+  width="100" height="100">
+  <!--
+${copyright}
+  -->
+  <defs>
+    <font id="${getInfo(font.names.fullName)}"
+      horiz-adv-x="${font.tables.hhea.advanceWidthMax}" vert-adv-y="${font.unitsPerEm}" >
+    <font-face font-family="${getInfo(font.names.fontFamily)}" font-weight="400"
+      font-stretch="normal"
+      units-per-em="${font.unitsPerEm}"
+      ascent="${font.ascender}"
+      descent="${font.descender}"/>
+`;
+  return header;
+}
+
+function glyphFooter() {
+  return `
+    </font>
+  </defs>
+</svg>`;
+}
+
+function toSVGFont(font, targetGlyphs) {
+  return glyphHeader(font) + toGlyphTag(font, targetGlyphs) + glyphFooter();
+}
+
+function toGlyphTag(font, glyphs) {
+  return glyphs.map((glyph) => {
+    const d = svgpath(glyph.path.toPathData())
+      .scale(1, -1)
+      .translate(0, font.ascender)
+      .toString();
+    return `<glyph glyph-name="&#${glyph.unicode};" unicode="&#${glyph.unicode};"
+      horiz-adv-x="${glyph.advanceWidth}" vert-adv-y="${font.unitsPerEm}"
+      d="${d}"/>`;
+  }).join("\n");
+}
 
 function ttf2svg(ttfPath, words) {
-  const transFont = fontCarrier.transfer(ttfPath);
+  const font = opentype.loadSync(ttfPath);
   if (words) {
     if (words.length == 1) {
-      const glyph = transFont.getGlyph(words);
-      console.log(glyph.toSvg());
+      const glyph = font.charToGlyph(words);
+      console.log(toSVG(font, glyph));
     } else {
-      const font = fontCarrier.create();
-      words.split("").forEach((word) => {
-        const glyph = transFont.getGlyph(word);
-        font.setGlyph(word, {
-          glyphName: word,
-          svg: glyph.toSvg(),
+      const glyphs = words.split("")
+        .map((word) => {
+          const targetGlyph = font.charToGlyph(word);
+          const codePoint = word.codePointAt(0);
+          const glyph = new opentype.Glyph({
+            // name: word,
+            unicode: codePoint,
+            glyphName: codePoint,
+            advanceWidth: targetGlyph.advanceWidth,
+            path: targetGlyph.path,
+          });
+          return glyph;
         });
-      });
-      const output = font.output({
-        types: ["svg"],
-      });
-      console.log(output.svg.toString());
+      console.log(toSVGFont(font, glyphs));
     }
   } else {
-    const output = transFont.output({
-      types: ["svg"],
+    // TODO: multiple missing-glyphs
+    const glyphs = Object.values(font.glyphs.glyphs);
+    const targetGlyphs = [];
+    glyphs.forEach((glyph) => {
+      if (glyph.unicode) {
+        // glyph.name = String.fromCodePoint(glyph.unicode);
+        targetGlyphs.push(glyph);
+      }
     });
-    console.log(output.svg.toString());
+    console.log(toSVGFont(font, targetGlyphs));
   }
 }
 
