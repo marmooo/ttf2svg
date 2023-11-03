b6b16d88f13bd3bc1ada4288bc0d8ff60e27a8ec
marmooo <marmooo@users.noreply.github.com>
2023-11-03 20:19:44 +0900

    v0.0.9

--- build_npm.js
+++ build_npm.js
@@ -17,7 +17,7 @@ await build({
   },
   package: {
     name: "@marmooo/ttf2svg",
-    version: "0.0.8",
+    version: "0.0.9",
     description: "Convert TTF to SVG.",
     license: "MIT",
     repository: {
