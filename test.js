11181360b005a3b2da3bdcda340c96b722912fc0
marmooo <marmooo@users.noreply.github.com>
2026-02-15 09:53:38 +0900

    improve tests

--- test.js
+++ test.js
@@ -1,13 +1,35 @@
-import { ttf2svg, ttf2svgFont } from "./mod.js";
-import { assertEquals, assertInstanceOf } from "@std/assert";
+import { ttf2svg } from "./mod.js";
+import { assert } from "@std/assert";
 
-Deno.test("ttf2svg check", () => {
-  const file = Deno.readFileSync("./test/Roboto-Regular.ttf");
-  const svg = ttf2svg(file);
-  assertInstanceOf(svg, Array);
+const roboto = Deno.readFileSync("./test/Roboto-Regular.ttf");
+const materialIcons = Deno.readFileSync("./test/material-icons.ttf");
+Deno.test("text check", () => {
+  const svg = ttf2svg(materialIcons, { text: "a" });
+  assert(svg.length === 1);
+  const svgs = ttf2svg(roboto);
+  assert(svgs.length !== 1);
 });
-Deno.test("ttf2svgFont check", () => {
-  const file = Deno.readFileSync("./test/Roboto-Regular.ttf");
-  const svg = ttf2svgFont(file);
-  assertEquals(typeof svg, "string");
+Deno.test("code 10 check", () => {
+  const svg = ttf2svg(materialIcons, { code: "59530" });
+  assert(svg.length === 1);
+  const svgs = ttf2svg(materialIcons);
+  assert(svgs.length !== 1);
+});
+Deno.test("code 16 check", () => {
+  const svg = ttf2svg(materialIcons, { code: "0xe88a" });
+  assert(svg.length === 1);
+  const svgs = ttf2svg(materialIcons);
+  assert(svgs.length !== 1);
+});
+Deno.test("name check", () => {
+  const svg = ttf2svg(materialIcons, { name: "home" });
+  assert(svg.length === 1);
+  const svgs = ttf2svg(materialIcons);
+  assert(svgs.length !== 1);
+});
+Deno.test("ligature check", () => {
+  const svg = ttf2svg(materialIcons, { ligature: "home" });
+  assert(svg.length === 1);
+  const svgs = ttf2svg(materialIcons);
+  assert(svgs.length !== 1);
 });
